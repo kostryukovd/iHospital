e3554a3f960edb82e88f87b187afa4c58a69cc24
kostryukovd <kostryukovd@gmail.com>
2018-09-21 16:26:04 +0300

    adaptive

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+
+function expand_menu() {
+
+console.log("test");
+
+$(".header-menu__lines").toggleClass("header-menu__lines-rotate");
+
+$(".hidden-menu").toggleClass("hidden-menu__show");
+
+}
+
+
